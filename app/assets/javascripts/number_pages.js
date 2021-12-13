2cedcb75ce05a9ca77923fdc06e78fa47dddd310
stap780 <panaet80@mail.ru>
2021-12-13 10:50:03 +0300

    change pdf

--- app/assets/javascripts/number_pages.js
+++ app/assets/javascripts/number_pages.js
@@ -1,31 +1,20 @@
 function check_first_page() {
-
+  var vars = {};
   var x = document.location.search.substring(1).split('&');
-  //document.getElementById("pdf_console_output").textContent += x;
-
   for (var i in x) {
-    //console.log('hi');
     var pagetype = x[i].split('=', 2);
-    if (pagetype[0] == "page") {
-      pagenumber = Number(x[i].split('=', 2)[1])
-      if (pagenumber == 1) {
-        document.getElementsByClassName("headerContent")[0].style.display = "none";
-      }
-      /*
-              if(pagenumber == 2 ){
-              document.getElementsByClassName("headerContent")[0].style.display = "block";
-              }
-              if(pagenumber == 3 ){
-              document.getElementsByClassName("headerContent")[0].style.display = "block";
-              }
-              if(pagenumber == 4 ){
-              document.getElementsByClassName("headerContent")[0].style.display = "block";
-              }
-              if(pagenumber == 5 ){
-              document.getElementsByClassName("headerContent")[0].style.display = "block";
-              }
-      */
-    }
+    vars[pagetype[0]] = decodeURIComponent(pagetype[1]);
+    // var pagetype_text = document.createTextNode(x[i]);
+    // document.getElementById("pdf_console_output").appendChild(pagetype_text);
+    // document.getElementById("pdf_console_output").style.display = 'block';
+  }
+  var vars_text = document.createTextNode(vars['page']);
+  document.getElementById("pdf_console_output").appendChild(vars_text);
+  document.getElementById("pdf_console_output").style.display = 'block';
+
+  var pagenumber = vars_text
+  if (pagenumber == 1) {
+    document.getElementsByClassName("headerContent")[0].style.display = "none";
   }
 }
 
@@ -45,18 +34,26 @@ function number_pages() {
     console.log(y);
     for (var j = 0; j < y.length; ++j) y[j].textContent = vars[x[i]];
   }
+  var pagenumber = vars['page']
+  if (pagenumber == 1) {
+    document.getElementsByClassName("headerContent")[0].style.display = "none";
+    document.getElementsByClassName("footerContent")[0].style.display = "none";
+  }
+
 }
 
 function loadPage() {
   document.getElementById('status').style.display = 'block';
 
-  document.getElementById("pdf_console_output").style.display = 'block';;
+  document.getElementById("pdf_console_output").style.display = 'block';
   document.getElementById("pdf_console_output").style.color = 'blue';
-  // var text = document.createTextNode(" - This just got added - ");
-  // document.getElementById("pdf_console_output").appendChild(text);
+  var text = document.createTextNode(" - This just got added - ");
+  document.getElementById("pdf_console_output").appendChild(text);
+
   var x = document.location.search.substring(1).split('&');
   for (var i in x) {
     var pagetype = x[i].split('=', 2);
-    document.getElementById("pdf_console_output").appendChild(pagetype);
+    var pagetype_text = document.createTextNode(pagetype);
+    document.getElementById("pdf_console_output").appendChild(pagetype_text);
   }
 }
--- app/controllers/kps_controller.rb
+++ app/controllers/kps_controller.rb
@@ -76,22 +76,24 @@ class KpsController < ApplicationController
 
   def print1
     @kp = Kp.find(params[:id])
-    puts @kp.present?
+    # puts @kp.present?
+    @our_company = Company.where(our_company: true).first
     @company = @kp.order.company
     respond_to do |format|
       format.html
       format.pdf do
           render :pdf => "КП1 #{@kp.id}",
-                 :template => "kps/print1",
+                 :template => "kps/print1.html.erb",
                  :show_as_html => params.key?('debug'),
-     						 :margin => {top: 12, left: 5, right: 5, bottom: 15 },
+     						 :margin => {top: 15, left: 0, right: 0, bottom: 20 },
      						 header:  {
-     						 		html: { template:'kps/print1_header'},
-     						 		:spacing => 3,
-     						 		locals: {}
+     						 		html: { template:'kps/print1_header.html.erb'},
+     						 		spacing: 3
+     						 		# locals: {},
+                    # right: 'Стр [page] из [topage]'
      						 		},
      						 footer: {
-     							 html: { template:'kps/print1_footer'},
+     							 html: { template:'kps/print1_footer.html.erb'},
      							 	:spacing => 3,
      								locals: {}
      								#right: '_______________________(подпись)                  _______________________(подпись)            [page] из [topage]'
