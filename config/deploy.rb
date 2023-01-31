ae4a661b02677c5f85b9ff5b92b73e0ae04ba1d4
stap780 <panaet80@mail.ru>
2023-01-31 16:18:21 +0300

    change deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :application, "alexproduct"
 set :repo_url, "git@github.com:stap780/#{fetch(:application)}.git"
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/pdf', 'vendor/bundle', 'public/system', 'public/excel_price', 'storage', 'public/tmp/pdf', 'lib/tasks')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/pdf', 'vendor/bundle', 'public', 'storage', 'lib/tasks')
 set :format, :pretty
 set :log_level, :info
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
