8afe88660c947b1770183eb41b1f82f89a5863dd
stap780 <panaet80@mail.ru>
2023-02-06 14:54:46 +0300

    update cron

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,24 +44,24 @@ class ProductsController < ApplicationController
 
   # PATCH/PUT /products/1
   def update
-  respond_to do |format|
-    if @product.update(product_params)
-      format.html { redirect_to @product, notice: "Product was successfully updated." }
-      format.json { render :show, status: :ok, location: @product }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @product.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: "Product was successfully updated." }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
     end
-  end
   end
 
   # DELETE /products/1
   def destroy
-  @product.destroy
-  respond_to do |format|
-    format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-    format.json { head :no_content }
-  end
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
 # POST /products
